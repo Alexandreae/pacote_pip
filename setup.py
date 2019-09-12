f38f0c682c9683dcf1c772196b8ec33c8a9f94ea
Alexandreae <alexandreae@al.insper.edu.br>
2019-09-12 08:30:57 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='my_hello_alexandreae',
+      author='Alexandre Edington',
+      version='0.1',
+      packages=['my_hello'],
+      python_requires='>=3.6',
+      classifiers=[
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3'
+          ],
+      install_requires=[
+          'Ipython>1.0'
+      ]
+      )     
